80a7fc2dd99c022760de162b59a41d35815ffd03
zaid-nawaz <zaidnawaz0005@gmail.com>
2020-08-31 01:50:02 -0700

    changed the password of sql

--- partials/dbconnect.php
+++ partials/dbconnect.php
@@ -3,7 +3,7 @@
 
 $servername = 'localhost';
 $username = 'root';
-$password = '';
+$password = '19712131607';
 $database = 'forum';
 
 $conn = mysqli_connect($servername,$username,$password,$database);
